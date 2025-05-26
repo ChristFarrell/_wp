3911700808bf3f1a4d5508755b04d5aa04372979
ChristFarr <christianofarrellrachman@gmail.com>
2025-05-26 15:33:46 +0800

    commit

--- MidTerm Project/server.js
+++ MidTerm Project/server.js
@@ -1,11 +1,8 @@
-// server.js (Deno + Oak)
 import { Application, send } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const app = new Application();
 
-// Static file middleware
 app.use(async (ctx) => {
-  // Serve index or any static file
   await send(ctx, ctx.request.url.pathname, {
     root: Deno.cwd(),
     index: "home.html",
